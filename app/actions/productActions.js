d2e6c2778043b0e2052b5b681917765201ef22e5
alexnm <alex.n.moldovan@gmail.com>
2016-08-28 18:11:40 +0300

    import * as syntax

--- app/actions/productActions.js
+++ app/actions/productActions.js
@@ -1,42 +1,34 @@
-import {
-    ADD_TO_CART,
-    GET_PRODUCT,
-    GET_PRODUCT_COMPLETED,
-    GET_PRODUCT_FAILED,
-    GET_PRODUCTS,
-    GET_PRODUCTS_COMPLETED,
-    GET_PRODUCTS_FAILED,
-} from "../actionIdentifiers";
+import * as Actions from "../actionIdentifiers";
 
 export const addToCart = product => ( {
-    type: ADD_TO_CART,
+    type: Actions.ADD_TO_CART,
     product,
 } );
 
 export const fetchProduct = {
-    name: GET_PRODUCT,
+    name: Actions.GET_PRODUCT,
     started: ( ) => ( {
-        type: GET_PRODUCT,
+        type: Actions.GET_PRODUCT,
     } ),
     completed: response => ( {
-        type: GET_PRODUCT_COMPLETED,
+        type: Actions.GET_PRODUCT_COMPLETED,
         product: response.product,
     } ),
     failed: ( ) => ( {
-        type: GET_PRODUCT_FAILED,
+        type: Actions.GET_PRODUCT_FAILED,
     } ),
 };
 
 export const fetchProducts = {
-    name: GET_PRODUCTS,
+    name: Actions.GET_PRODUCTS,
     started: ( ) => ( {
-        type: GET_PRODUCTS,
+        type: Actions.GET_PRODUCTS,
     } ),
     completed: response => ( {
-        type: GET_PRODUCTS_COMPLETED,
+        type: Actions.GET_PRODUCTS_COMPLETED,
         products: response.products,
     } ),
     failed: ( ) => ( {
-        type: GET_PRODUCTS_FAILED,
+        type: Actions.GET_PRODUCTS_FAILED,
     } ),
 };
